9bb6dc990612236ecc57e8d446d4f9f6fc260de7
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-08-11 23:15:08 +0300

    client-typing

--- src/app.ts
+++ src/app.ts
@@ -15,25 +15,51 @@ app.get('/', (req, res) => {
     res.send('Hello it\'s ws server');
 });
 
-const messages = [
-    {id: '12j12', message: 'Hello Viktor', user: {id: '2323kkkk23', name: 'Dimych'}},
-    {id: '1222j12', message: 'Hello Dimych', user: {id: '23221kk23', name: 'Viktor'}}
-]
+const messages: Array<any> = []
+
+const usersState = new Map()
+
 
 socket.on('connection', (socketChanel) => {
 
-    socketChanel.on('client-message-sent', (message: string) => {
+    usersState.set(socketChanel, {id: new Date().getTime().toString(), name: 'anonymous'})
+
+    socket.on('disconnect', () => {
+        usersState.delete(socketChanel)
+    })
+
+    socketChanel.on('client-name-sent', (name: string) => {
+        const user = usersState.get(socketChanel)
+        user.name = name
+    })
+
+    socketChanel.on('client-typing', () => {
+        socketChanel.broadcast.emit('user-typing', usersState.get(socketChanel))
+    })
+
+    socketChanel.on('client-message-sent', (message: string, successFn) => {
+        if (typeof message !== 'string' || message.length > 20) {
+            successFn('Message length should be less than 20 chars')
+            return
+        }
+
+        const user = usersState.get(socketChanel)
+
         let messageItem = {
-            id: '1222j12',
+            id: new Date().getTime().toString(),
             message: message,
-            user: {id: '23221' + new Date().getTime(), name: 'Viktor'}
+            user: {id: user.id, name: user.name}
         }
         messages.push(messageItem)
 
         socket.emit('new-message-sent', messageItem)
+
+        successFn(null)
     });
 
-    socketChanel.emit('init-messages-published', messages)
+    socketChanel.emit('init-messages-published', messages, () => {
+        console.log('init messages received')
+    })
 
     console.log('a user connected');
 });
